02bdb76640b6cba6c58fab466c6d8662bd9dc183
Daniel Kelly <me@danielkelly.io>
2022-01-14 11:08:41 -0600

    init commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import TheHeader from "@/components/TheHeader.vue";
+import ProductCard from "@/components/ProductCard.vue";
+import products from "@/data/products.json";
+</script>
+
+<template>
+  <div class="container">
+    <TheHeader />
+    <ul class="md:flex gap-5">
+      <ProductCard
+        v-for="product in products"
+        :key="product.name"
+        :product="product"
+      />
+    </ul>
+  </div>
+</template>
